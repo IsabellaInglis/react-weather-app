74176aedc63a18b44e7a1ed44c85cb5d8bbbe1df
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-07-16 10:17:02 +0100

    Fixed dependencies error and added links

--- src/Weather.js
+++ src/Weather.js
@@ -7,11 +7,19 @@ export default function Weather() {
     <div className="weather-container">
       <h1>London</h1>
       17ºC Humidity: 79% Wind: 10mph
-      <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+      <img
+        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+        alt="weather-icon"
+      />
       <footer>
         This project was coded by{" "}
         <a href="https://github.com/IsabellaInglis">Isabella Inglis</a>,
-        open-sourced on Github and hosted on Netlify
+        open-sourced on{" "}
+        <a href="https://github.com/IsabellaInglis/react-weather-app">Github</a>{" "}
+        and hosted on{" "}
+        <a href="https://react-application-weather-forecast.netlify.app/">
+          Netlify
+        </a>
       </footer>
     </div>
   );
