0c10f68ca38a7c25afa6c3d474dc0f1ca1f6ee52
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-07-20 15:07:22 +0100

    Added axios and imported weather api

--- src/Weather.js
+++ src/Weather.js
@@ -1,39 +1,68 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <input
-          type="search"
-          placeholder="Enter a city..."
-          className="form-control"
-          autoFocus="on"
-        />
-        <input type="submit" value="Search" className="btn btn-primary" />
-      </form>
-      <hr />
-      <h1>London</h1>
-      <ul>
-        <li>Tuesday, 10:00</li>
-        <li>Mostly cloudy</li>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="weather-icon"
-            />
-            <span className="temperature-value">17</span>
-            <span className="temperature-unit">ºC</span>
-          </div>
-          <div className="col-6">
-            <li>Humidity: 79%</li>
-            <li>Wind: 10mph</li>
-          </div>
-        </div>
-      </ul>
-    </div>
+  const [weatherData, setWeatherData] = useState({ loaded: false });
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        className="form-control"
+        autoFocus="on"
+      />
+      <input type="submit" value="Search" className="btn btn-primary" />
+    </form>
   );
+
+  function handleResponse(response) {
+    setWeatherData({
+      loaded: true,
+      temperature: Math.round(response.data.temperature.current),
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.city,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const apiKey = "dc6760cf7088c245e5a42a646bco203t";
+    let city = "London";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <hr />
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Tuesday, 10:00</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+          <div className="row">
+            <div className="col-6">
+              <img src={weatherData.icon} alt={weatherData.description} />
+              <span className="temperature-value">
+                {weatherData.temperature}
+              </span>
+              <span className="temperature-unit">ºC</span>
+            </div>
+            <div className="col-6">
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} mph</li>
+            </div>
+          </div>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div className="Weather">{form}</div>;
+  }
 }
