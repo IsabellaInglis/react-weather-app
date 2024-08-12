7460c9da14add942fdcff2db0938db0956065d8d
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-08-12 12:16:57 +0100

    Updated footer links

--- src/App.js
+++ src/App.js
@@ -9,11 +9,27 @@ function App() {
       </div>
       <footer>
         This project was coded by{" "}
-        <a href="https://github.com/IsabellaInglis">Isabella Inglis</a>,
-        open-sourced on{" "}
-        <a href="https://github.com/IsabellaInglis/react-weather-app">Github</a>{" "}
+        <a
+          href="https://github.com/IsabellaInglis"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Isabella Inglis
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/IsabellaInglis/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
         and hosted on{" "}
-        <a href="https://react-application-weather-forecast.netlify.app/">
+        <a
+          href="https://react-application-weather-forecast.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netlify
         </a>
       </footer>
